f055af32e70ef88d834cd3db192b7e4081b3b820
Piyush Saini <findpiyushsaini@gmail.com>
2024-06-29 01:59:12 +0530

    Improve data preprocessing and model training, and  output readability

--- nmt.py
+++ nmt.py
@@ -1,12 +1,20 @@
+#original model link: https://github.com/priyadarshiguha/English-to-Hindi-NMT-Model-Using-RNN
+
 import numpy as np
 from keras.models import Model
 from keras.layers import Input, LSTM, Embedding, Dense
 from keras.optimizers import Adam
+from keras.preprocessing.sequence import pad_sequences
+
+import time
+
+start_time = time.time()
 
 data = open(r"C:\Users\admin\CCPS Research\virtual_test\piyush\eng_to_hin.txt", 'r', encoding='utf8', errors='ignore').read()
 lines = data.split('\n')
 lines = lines[:]
 
+#=--------------------------
 english = [l.split('\t')[0] for l in lines]
 hindi = [l.split('\t')[1] for l in lines]
 hindi = ['<s> '+h+' <e>' for h in hindi]
@@ -14,7 +22,7 @@ hindi = ['<s> '+h+' <e>' for h in hindi]
 english_vocab = sorted(list(set([w for e in english for w in e.split()])))
 hindi_vocab = sorted(list(set([w for h in hindi for w in h.split()])))
 
-english_vocab_size = min(8000, len(english_vocab)) #vocab size
+english_vocab_size = min(8000, len(english_vocab))
 hindi_vocab_size = min(8000, len(hindi_vocab))
 
 english_vocab = english_vocab[:english_vocab_size]
@@ -28,18 +36,18 @@ hindi_w_to_i = {w: i for i, w in enumerate(hindi_vocab)}
 english_i_to_w = {i: w for i, w in enumerate(english_vocab)}
 hindi_i_to_w = {i: w for i, w in enumerate(hindi_vocab)}
 
-encoder_input_data = np.zeros((len(english), max_english_len), dtype='float32') #specifying datatype 
-decoder_input_data = np.zeros((len(hindi), max_hindi_len), dtype='float32')
-decoder_target_data = np.zeros((len(hindi), max_hindi_len, hindi_vocab_size), dtype='float32')
+#-----------------
+def sentences_to_indices(sentences, word_to_index, max_len):
+    indices = [[word_to_index.get(w, 0) for w in s.split()] for s in sentences]
+    return pad_sequences(indices, maxlen=max_len, padding='post')
 
-for i in range(len(english)):
-    for j in range(len(english[i].split())):
-        encoder_input_data[i, j] = english_w_to_i.get(english[i].split()[j], 0)
-for i in range(len(hindi)):
-    for j in range(len(hindi[i].split())):
-        decoder_input_data[i, j] = hindi_w_to_i.get(hindi[i].split()[j], 0)
-        if j > 0:
-            decoder_target_data[i, j - 1, hindi_w_to_i.get(hindi[i].split()[j], 0)] = 1.0
+encoder_input_data = sentences_to_indices(english, english_w_to_i, max_english_len)
+decoder_input_data = sentences_to_indices(hindi, hindi_w_to_i, max_hindi_len)
+
+decoder_target_data = np.zeros((len(hindi), max_hindi_len, hindi_vocab_size), dtype='float32')
+for i, seq in enumerate(decoder_input_data):
+    for t in range(1, len(seq)):
+        decoder_target_data[i, t - 1, seq[t]] = 1.0
 
 units = 256
 epochs = 100
@@ -47,14 +55,14 @@ batch_size = 32
 
 encoder_inputs = Input(shape=(None,))
 encoder_embedding = Embedding(english_vocab_size, units)(encoder_inputs)
-encoder_LSTM = LSTM(units, return_sequences=True, return_state=True)
+encoder_LSTM = LSTM(units, return_sequences=False, return_state=True)
 _, state_h, state_c = encoder_LSTM(encoder_embedding)
 encoder_states = [state_h, state_c]
 
 decoder_inputs = Input(shape=(None,))
 decoder_embedding_layer = Embedding(hindi_vocab_size, units)
 decoder_embedding = decoder_embedding_layer(decoder_inputs)
-decoder_LSTM = LSTM(units, return_state=True, return_sequences=True)
+decoder_LSTM = LSTM(units, return_sequences=True, return_state=True)
 decoder_outputs, _, _ = decoder_LSTM(decoder_embedding, initial_state=encoder_states)
 decoder_dense = Dense(hindi_vocab_size, activation='softmax')
 decoder_outputs = decoder_dense(decoder_outputs)
@@ -67,9 +75,7 @@ model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
           epochs=epochs, batch_size=batch_size, verbose=1)
 
 weights = 'nmt_weights.weights.h5'
-
 model.save_weights(weights)
-
 model.load_weights(weights)
 
 encoder_model = Model(encoder_inputs, encoder_states)
@@ -85,14 +91,15 @@ decoder_states_2 = [state_h_2, state_c_2]
 
 decoder_model = Model([decoder_inputs]+decoder_state_inputs, [decoder_output_2]+decoder_states_2)
 
+train_time = time.time()
+print(f"Train time taken1: {train_time - start_time} seconds")
 
 def translate(input_seq):
-    #batch dimension
     input_seq = input_seq.reshape(1, -1)
-    states = encoder_model.predict(input_seq) #encoder states
+    states = encoder_model.predict(input_seq)
     
-    target_seq = np.zeros((1, 1)) #input for decoder
-    target_seq[0, 0] = hindi_w_to_i['<s>']
+    target_seq = np.zeros((1, 1))
+    target_seq[0, 0] = hindi_w_to_i.get('<s>', 0)
     
     stop = False
     target_sentence = ''
@@ -106,13 +113,14 @@ def translate(input_seq):
         if char == '<e>' or len(target_sentence.split()) > max_hindi_len:
             stop = True
         
-        target_seq = np.zeros((1, 1)) 
-        target_seq[0, 0] = index #updating target sequence
+        target_seq = np.zeros((1, 1))
+        target_seq[0, 0] = index
         
-        states = [h, c] #updating states
+        states = [h, c]
     
-    return target_sentence
+    return target_sentence.strip()
 
+results = []
 for i in range(len(english)):
     print("-----------------------------------------------------------------------------------------------")
     print('English Sentence: ', english[i])
@@ -124,3 +132,21 @@ for i in range(len(english)):
     input_seq2 = encoder_input_data[i, :].reshape(1, -1)
     predicted_sentence2 = translate(input_seq2)
     print('Predicted Hindi Sentence2: ', predicted_sentence2)
+
+    results.append((english[i], hindi[i], predicted_sentence2))
+
+
+train_time2 = time.time()
+print(f"Train time taken2: {train_time2 - start_time} seconds")
+
+
+#----------------------------better print
+for i, (eng, target, pred) in enumerate(results):
+    print(f"\nSample {i + 1}")
+    print(f"English: {eng}")
+    print(f"Target Hindi: {target}")
+    print(f"Predicted Hindi: {pred}")
+   
+    
+end_time = time.time()
+print(f"Total time taken: {end_time - start_time} seconds")
